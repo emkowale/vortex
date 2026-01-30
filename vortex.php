07b6e706790bbf14e65c9d8c1836fefbe4b2ab96
Eric Kowalewski <eric@thebeartraxs.com>
2026-01-30 09:53:52 -0500

    Release v1.0.5

--- vortex.php
+++ vortex.php
@@ -3,7 +3,7 @@
  * Plugin Name: Vortex - Shareable Carts
  * Plugin URI:  https://github.com/emkowale/vortex
  * Description: Create a short link from the current cart that pre-fills WooCommerce cart for others.
- * Plugin 	.0.4
+ * 	.0.5
  * Author:      Eric Kowalewski
  * Author URI:  https://github.com/emkowale
  * License:     GPL-2.0+
